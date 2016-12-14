9e836655c201c78e85aff383dfb1a37ab0845935
Peter-Frank Spierenburg <peter.spierenburg@nautel.com>
2016-12-14 09:44:47 -0400

    Added stuff to json_interpreter_test

--- goal_light/json_interpreter.py
+++ goal_light/json_interpreter.py
@@ -2,14 +2,21 @@ import logging
 import simplejson
 
 class JsonInterpreter:
-    def __init__(self, light=None, horn=None):
+    def __init__(self, light=None, horn=None, downloader=None, button_actions=None):
         self.light = light
         self.horn = horn
+        self.downloader = downloader
+        self.button_actions = button_actions
 
     def interpret(self, json):
         dict = simplejson.loads(json)
         logging.info("Received: " + str(dict))
-        if 'l' in dict:
+        if 'l' in dict and dict['l'] != "0.0":
             self.light.trigger(float(dict['l']))
-        if 'h' in dict:
-            self.horn.trigger("horn/"+dict['h']+".mp3")
+        if 'h' in dict and dict['h'] != "0":
+            self.horn.trigger(dict['h'])
+        if 'dl' in dict and dict['dl'] != "0":
+            self.downloader.trigger(dict['dl'])
+        if 'b' in dict and dict['b'] != "0":
+            self.button_actions.update_config(dict['b'])
+        return int(dict['d']) if 'd' in dict else 300
--- tests/json_interpreter_test.py
+++ tests/json_interpreter_test.py
@@ -1,15 +1,46 @@
 from .context import goal_light
 
 class MockLight:
+    def __init__(self):
+        self.duration = None
+
     def trigger(self, duration):
         self.duration = duration
 
 class MockHorn:
+    def __init__(self):
+        self.horn = None
+        
     def trigger(self, horn):
-        self.horn = horn
+        self.horn = "horn/" + horn + ".mp3"
+
+class MockDownloader:
+    def __init__(self):
+        self.url = None
+        self.destination = None
+        
+    def trigger(self, id):
+        self.url = "http://lh.meetcweb.com/goal/horns/" + id + ".mp3"
+        self.destination = "horn/" + id + ".mp3"
+
+class MockButtonActions:
+    def __init__(self):
+        self.config = None
+
+    def update_config(self, config):
+        print "Updated config: " + str(config)
+        self.config = config
 
 def test_construct_json_interpreter():
     interpreter = goal_light.JsonInterpreter()
+
+def test_poll_delay():
+    interpreter = goal_light.JsonInterpreter()
+    assert 100 == interpreter.interpret('{"d":"100"}')
+
+def test_no_poll_delay():
+    interpreter = goal_light.JsonInterpreter()
+    assert 300 == interpreter.interpret('{}')
 
 def test_trigger_light():
     light = MockLight()
@@ -17,8 +48,46 @@ def test_trigger_light():
     interpreter.interpret('{"l":"5.0"}')
     assert light.duration == 5.0
 
+def test_no_trigger_light():
+    light = MockLight()
+    interpreter = goal_light.JsonInterpreter(light)
+    interpreter.interpret('{"l":"0.0"}')
+    assert light.duration == None
+
 def test_trigger_horn():
     horn = MockHorn()
     interpreter = goal_light.JsonInterpreter(None, horn)
     interpreter.interpret('{"h":"8"}')
     assert horn.horn == "horn/8.mp3"
+
+def test_no_trigger_horn():
+    horn = MockHorn()
+    interpreter = goal_light.JsonInterpreter(None, horn)
+    interpreter.interpret('{"h":"0"}')
+    assert horn.horn == None
+
+def test_trigger_download():
+    downloader = MockDownloader()
+    interpreter = goal_light.JsonInterpreter(None, None, downloader)
+    interpreter.interpret('{"dl":"82"}')
+    assert downloader.url == "http://lh.meetcweb.com/goal/horns/82.mp3"
+    assert downloader.destination == "horn/82.mp3"
+
+def test_no_trigger_download():
+    downloader = MockDownloader()
+    interpreter = goal_light.JsonInterpreter(None, None, downloader)
+    interpreter.interpret('{"dl":"0"}')
+    assert downloader.url == None
+    assert downloader.destination == None
+
+def test_configure_buttons():
+    button_actions = MockButtonActions()
+    interpreter = goal_light.JsonInterpreter(None, None, None, button_actions)
+    interpreter.interpret('{"b":{"3h":"5","3l":"6.0","2l":"4.0","1h":"1","2h":"3","1l":"2.0"}}')
+    assert button_actions.config != None
+
+def test_no_configure_buttons():
+    button_actions = MockButtonActions()
+    interpreter = goal_light.JsonInterpreter(None, None, None, button_actions)
+    interpreter.interpret('{"b":"0"}')
+    assert button_actions.config == None
