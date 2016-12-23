b8d7142950014d6bd28ba61dfcacc21a00c18665
Peter-Frank Spierenburg <spierepf@hotmail.com>
2016-12-23 11:24:54 -0400

    Horn now makes noise

--- goal_light/horn.py
+++ goal_light/horn.py
@@ -1,8 +1,20 @@
 import logging
+import vlc
 
 class Horn:
     def __init__(self):
-        pass
+        self.horn_sound = None
+
+    def silence_horn(self):
+        if self.horn_sound != None:
+            self.horn_sound.stop()
+            self.horn_sound = None
+
+    def sound_horn(self, filename):
+        self.silence_horn()
+        self.horn_sound = vlc.MediaPlayer(filename)
+        self.horn_sound.play()
 
     def trigger(self, id):
         logging.info("Triggering horn: " + id)
+        self.sound_horn("horn/" + str(id) + ".mp3")
