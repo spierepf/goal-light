0a3136e6ae3f32fcd398b11edbccb263549122d7
Peter-Frank Spierenburg <spierepf@hotmail.com>
2017-01-03 18:10:29 -0400

    Adding gpio goodness to light

--- goal_light/light.py
+++ goal_light/light.py
@@ -1,8 +1,12 @@
 import logging
+import RPi.GPIO as GPIO
 
 class Light:
     def __init__(self):
-        pass
+        GPIO.setup(self.light_gpio, GPIO.OUT)
 
     def trigger(self, duration):
         logging.info("Triggering light for " + str(float(duration)) + " seconds")
+        GPIO.output(self.light_gpio, 1)
+        time.sleep(duration)
+        GPIO.output(self.light_gpio, 0)
--- tests/light_test.py
+++ tests/light_test.py
@@ -1,5 +1,13 @@
 from .context import goal_light
 
+import RPi.GPIO as GPIO
+
+def setup_module(module):
+    GPIO.setmode(GPIO.BOARD)
+
+def teardown_module(module):
+    GPIO.cleanup()
+
 def test_construct_light():
     light = goal_light.Light()
 
