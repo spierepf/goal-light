d92aeb55e918742264480e51c12863c3e542ff70
Nobody <nobody@nowhere.com>
2017-01-03 19:00:09 -0400

    Threaded light

--- goal_light/light.py
+++ goal_light/light.py
@@ -1,12 +1,30 @@
 import logging
+import threading
+
 import RPi.GPIO as GPIO
 
 class Light:
     def __init__(self):
-        GPIO.setup(self.light_gpio, GPIO.OUT)
+        self.cv = threading.Condition()
+
+    def thread_body(self, duration):
+        self.cv.acquire()
+
+	GPIO.setmode(GPIO.BOARD)
+	GPIO.setup(16, GPIO.OUT, initial=GPIO.HIGH)
+
+        self.cv.wait(duration)
+
+        GPIO.output(16, GPIO.LOW)
+        GPIO.cleanup()
+
+        self.cv.release()
 
     def trigger(self, duration):
-        logging.info("Triggering light for " + str(float(duration)) + " seconds")
-        GPIO.output(self.light_gpio, 1)
-        time.sleep(duration)
-        GPIO.output(self.light_gpio, 0)
+        if duration == None or duration == 0.0:
+            logging.info("Extinguishing light because duration zero")
+            self.cv.acquire()
+            self.cv.notify()
+            self.cv.release()
+        else:
+            threading.Thread(target=self.thread_body, args=(duration,)).start()
